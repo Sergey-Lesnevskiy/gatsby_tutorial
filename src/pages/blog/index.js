c0d17a10b62571e8a87698719a695ce6443030e3
Lesnevsky-Sergey <78030028+Sergey-Lesnevskiy@users.noreply.github.com>
2025-04-01 17:33:15 +0300

    Finished Gatsby Tutorial Part 6

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -1,23 +1,28 @@
 import * as React from 'react'
-import Layout from '../components/layout'
-import { graphql } from 'gatsby'
-import Seo from '../components/seo'
+import { Link, graphql } from 'gatsby'
+import Layout from '../../components/layout'
+import Seo from '../../components/seo'
 
 const BlogPage = ({ data }) => {
+  console.log(data);
   return (
     <Layout pageTitle="My Blog Posts">
       {
-        data.allMdx.nodes.map((node) => (
+        data.allMdx.nodes.map(node => (
           <article key={node.id}>
-            <h2>{node.frontmatter.title}</h2>
+            <h2>
+              <Link to={`/blog/${node.frontmatter.slug}`}>
+                {node.frontmatter.title}
+              </Link>
+            </h2>
             <p>Posted: {node.frontmatter.date}</p>
-            <p>{node.excerpt}</p>
           </article>
         ))
       }
     </Layout>
   )
 }
+
 export const query = graphql`
   query {
     allMdx(sort: { frontmatter: { date: DESC }}) {
@@ -25,13 +30,14 @@ export const query = graphql`
         frontmatter {
           date(formatString: "MMMM D, YYYY")
           title
+          slug
         }
         id
-        excerpt
       }
     }
   }
 `
+
 export const Head = () => <Seo title="My Blog Posts" />
 
 export default BlogPage
